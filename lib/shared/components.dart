e65d8816f42c5f2a6ca417b2b5698f6629183c29
Yehia Mohamed <102627389+YehiaFarghaly@users.noreply.github.com>
2022-11-29 01:28:58 +0200

    feat : Handling WebView and finishing the app

--- lib/shared/components.dart
+++ lib/shared/components.dart
@@ -1,53 +1,59 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_news_app/modules/web_view/web_screen.dart';
 
-Widget buildNewsArticle(article){
-  return Padding(
-    padding: const EdgeInsets.all(20.0),
-    child: Row(
-      children: [
-        Container(
-          width: 120,
-          height: 120,
-          decoration: BoxDecoration(
-            borderRadius: BorderRadius.circular(10.0),
-            image: DecorationImage(
-              image: NetworkImage('${article['urlToImage']}'),
-              fit: BoxFit.cover,
+Widget buildNewsArticle(article,context){
+  return InkWell(
+    onTap: (){
+      navigateTo(context, WebViewScreen(article['url']));
+    },
+    child: Padding(
+      padding: const EdgeInsets.all(20.0),
+      child: Row(
+        children: [
+          Container(
+            width: 120,
+            height: 120,
+            decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(10.0),
+              image: DecorationImage(
+                image: NetworkImage('${article['urlToImage']}'),
+                fit: BoxFit.cover,
+              ),
             ),
           ),
-        ),
-        SizedBox(
-          width: 20,
-        ),
-        Expanded(
-          child:Container(
-            height: 120,
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              mainAxisSize: MainAxisSize.max,
-              children: [
-                Expanded(
-                  child: Text(
-                    '${article['title']}',
-                    overflow: TextOverflow.ellipsis,
-                    maxLines: 3,
-                    style: TextStyle(
-                      fontSize: 16,
-                      fontWeight: FontWeight.bold,
-                      color: Colors.amber[800],
+          SizedBox(
+            width: 20,
+          ),
+          Expanded(
+            child:Container(
+              height: 120,
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                mainAxisSize: MainAxisSize.max,
+                children: [
+                  Expanded(
+                    child: Text(
+                      '${article['title']}',
+                      overflow: TextOverflow.ellipsis,
+                      maxLines: 3,
+                      style: TextStyle(
+                        fontSize: 16,
+                        fontWeight: FontWeight.bold,
+                        color: Colors.amber[800],
+                      ),
                     ),
                   ),
-                ),
-                Text('${article['publishedAt']}',
-                style: TextStyle(
-                  color: Colors.grey,
-                ),
-                ),
-              ],
+                  Text('${article['publishedAt']}',
+                  style: TextStyle(
+                    color: Colors.grey,
+                  ),
+                  ),
+                ],
+              ),
             ),
           ),
-        ),
-      ],
+        ],
+      ),
     ),
   );
 }
@@ -65,7 +71,7 @@ Widget articleBuilder(list){
       ? Center(child: CircularProgressIndicator())
       : ListView.separated(
       physics:BouncingScrollPhysics(),
-      itemBuilder: (context,index)=> buildNewsArticle(list[index]),
+      itemBuilder: (context,index)=> buildNewsArticle(list[index],context),
       separatorBuilder: (context,index) => myDivider(),
       itemCount: list.length);
 }
--- lib/shared/constants.dart
+++ lib/shared/constants.dart
@@ -1,5 +1,5 @@
 String BASE_URL="https://newsapi.org/";
 String PATH = "v2/top-headlines";
-String API_KEY="61afcece37114f38b9eec25ed733fb2c";
+String API_KEY="7dcdc959641e4f3681a161a94e0e4b50";
 String SEARCH_PATH='v2/everything';
 String SEARCH_QUERY ='q';
